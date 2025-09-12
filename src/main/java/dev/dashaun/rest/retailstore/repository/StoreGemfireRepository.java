569ce805946ebf6ecad7404a752b3f1438416a95
DaShaun Carter <dashaun@dashaun.com>
2025-09-11 19:23:00 -0500

    add ClientRegion

--- src/main/java/dev/dashaun/rest/retailstore/repository/StoreGemfireRepository.java
+++ src/main/java/dev/dashaun/rest/retailstore/repository/StoreGemfireRepository.java
@@ -3,5 +3,6 @@ package dev.dashaun.rest.retailstore.repository;
 import dev.dashaun.rest.retailstore.domain.StoreGemfire;
 import org.springframework.data.gemfire.repository.GemfireRepository;
 
+@ClientRegion("Stores")
 public interface StoreGemfireRepository extends GemfireRepository<StoreGemfire, String> {
 }
